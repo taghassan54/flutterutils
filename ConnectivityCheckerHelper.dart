23251f37e6df53b9bbcf2f58bd730c5d50937719
TAjELDEEN HASSAN <41596304+taghassan54@users.noreply.github.com>
2024-10-01 17:51:55 +0400

    Update ConnectivityCheckerHelper.dart

--- ConnectivityCheckerHelper.dart
+++ ConnectivityCheckerHelper.dart
@@ -1,1 +1,35 @@
 
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable @singleton
+class ConnectivityCheckerHelper {
+  Future<bool> checkConnectivity() async {
+    final ConnectivityResult connectivityResult =
+    await Connectivity().checkConnectivity();
+    return _handleResult(connectivityResult);
+  }
+
+  Future<ConnectivityResult> getConnectivityResult() async {
+    final ConnectivityResult connectivityResult =
+    await Connectivity().checkConnectivity();
+    return connectivityResult;
+  }
+
+   Stream<ConnectivityResult> listenToConnectivityChanged() {
+    return Connectivity().onConnectivityChanged;
+  }
+
+  bool _handleResult(ConnectivityResult connectivityResult) {
+    final bool connected;
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      connected = true;
+    } else {
+      connected = false;
+    }
+
+    return connected;
+  }
+}
